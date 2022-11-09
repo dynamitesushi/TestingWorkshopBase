0a3cf1fc6188aa8564ad2e6bdd699fffc476cf8a
dynamitesushi <winson.hero@gmail.com>
2022-11-09 11:18:44 +0000

    patch token create

--- src/minima/index.js
+++ src/minima/index.js
@@ -1,6 +1,6 @@
 export function tokenCreate(name, amount) {
     return new Promise((resolve, reject) => {
-        window.MDS.cmd(`tokencreate name:${encodeURIComponent(name)} amount:${amount}`, function (token) {
+        window.MDS.cmd(`tokencreate name:{"name":"${name}", "description":"test", "url":"", "ticker":""} amount:${amount}`, function (token) {
             if (token.response) {
                 return resolve(token.response);
             }
